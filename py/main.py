f0f9b53e4273e7920abd09aeea9d9c0e8c5f9214
Kelley <Bronson.Kelley@nestle.com>
2023-12-15 14:53:10 -0600

    updating code

--- py/main.py
+++ py/main.py
@@ -1,24 +1,32 @@
 import pandas as pd
 class DataCleaner:
-    def __init__(self, input_file, output_file):
-        self.input_file = input_file
-        self.output_file = output_file
-    def read_data(self):
-        self.df = pd.read_csv(self.input_file)
+    def __init__(self, data):
+        self.data = data
+    
     def remove_duplicates(self):
-        self.df = self.df.drop_duplicates()
+        self.data = self.data.drop_duplicates()
+    
     def remove_missing_values(self):
-        self.df = self.df.dropna()
-    def convert_to_lowercase(self, column_name):
-        self.df[column_name] = self.df[column_name].str.lower()
-    def remove_whitespaces(self, column_name):
-        self.df[column_name] = self.df[column_name].str.strip()  
-    def save_data(self):
-        self.df.to_csv(self.output_file, index=False)
-cleaner = DataCleaner('Electric_Vehicle_Population_data.csv', 'output.csv')
-cleaner.read_data()
-cleaner.remove_duplicates()
-cleaner.remove_missing_values()
-cleaner.convert_to_lowercase('County')
-cleaner.remove_whitespaces('County')
-cleaner.save_data()
+        self.data = self.data.dropna()
+    
+    def convert_all_to_lowercase(self, column):
+        self.data[column] = self.data[column].str.title()
+    
+    def convert_to_numeric(self, column):
+        self.data[column] = pd.to_numeric(self.data[column], errors='coerce')
+    
+    def clean_data(self):
+        self.remove_duplicates()
+        self.remove_missing_values()
+    
+    def get_cleaned_data(self):
+        return self.data
+data = pd.read_csv('Electric_Vehicle_Population_data.csv')
+cleaner = DataCleaner(data)
+cleaner.clean_data()
+cleaned_data = cleaner.get_cleaned_data()
+cleaner.convert_all_to_lowercase('Make')
+cleaner.convert_all_to_lowercase('Model')
+cleaner.convert_all_to_lowercase('Electric Utility')
+# Export cleaned data to a new CSV file
+cleaned_data.to_csv('cleaned_data.csv', index=False)
