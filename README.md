fe0cb01e4ca28beee0bf10273afb80356d993cb3
Kelley <Bronson.Kelley@nestle.com>
2023-12-22 14:44:10 -0600

    Updating code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # OC-Project3
+
+## How to run ETL
+
+To run the ETL pipeline you will need a few things installed:
+
+- [Python](https://www.python.org/downloads/)
+- Pip installed should come with the latest version of python
+- Pandas (is already installed in environment)
+
+You can either open the folder in a IDE like VSCode or Pycharm and run it manually there or from the command line by moving into the directory and running the command python .\main.py
+
+## How to Activate Virtual Environment
+
+- Clone the repository using git clone (url) this will give you all of the files in the repository
+- In the terminal you will cd into the Scripts folder and run .\activate and that will activate your environment
+
+## How to generate a flake8 file
+
+- First you need to install flake8 using pip install flake8
+- Create a directory you want to create the flake8 file in
+- Run the command python3 -m flake8 --docstring-convention google --format=html --htmldir=reports/flake
+- Open the index file in a browser and it will display your report
--- py/main.py
+++ py/main.py
@@ -1,32 +1,43 @@
+"""Imports pandas."""
 import pandas as pd
+
+
 class DataCleaner:
+    """Defines the class."""
+
     def __init__(self, data):
+        """Defines data."""
         self.data = data
-    
+
     def remove_duplicates(self):
+        """Removes duplicate rows."""
         self.data = self.data.drop_duplicates()
-    
+
     def remove_missing_values(self):
+        """Removes missing values."""
         self.data = self.data.dropna()
-    
-    def convert_all_to_lowercase(self, column):
+
+    def convert_to_lowercase(self, column):
+        """Converts values to title."""
         self.data[column] = self.data[column].str.title()
-    
-    def convert_to_numeric(self, column):
-        self.data[column] = pd.to_numeric(self.data[column], errors='coerce')
-    
+
     def clean_data(self):
+        """Cleans the data."""
         self.remove_duplicates()
         self.remove_missing_values()
-    
+
     def get_cleaned_data(self):
+        """Gets the cleaned data."""
         return self.data
-    
+
+
 data = pd.read_csv('Electric_Vehicle_Population_data.csv')
 cleaner = DataCleaner(data)
 cleaner.clean_data()
 cleaned_data = cleaner.get_cleaned_data()
-cleaner.convert_all_to_lowercase('Make')
-cleaner.convert_all_to_lowercase('Model')
-cleaner.convert_all_to_lowercase('Electric Utility')
-cleaned_data.to_csv('cleaned_data.csv', index=False)
+cleaner.convert_to_lowercase('Make')
+cleaner.convert_to_lowercase('Model')
+cleaner.convert_to_lowercase('Electric Utility')
+compression_opts = dict(method='zip',
+                        archive_name='cleaned_data.csv')
+cleaned_data.to_csv('cleaned_data.zip', compression=compression_opts)
